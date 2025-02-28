0667173ff3a2c601bdf5c84fbde288f9b4066ee7
MJMcManaman <158525695+MJMcManaman@users.noreply.github.com>
2025-02-27 20:33:10 -0500

    Mls2_Insertion.sql
    addded a insertion format for property

--- Mls2_Insertion.sql
+++ Mls2_Insertion.sql
@@ -16,6 +16,9 @@ insert into property values(property_t('d01',
 listing_t('d012', '5-Jan-2025', 2005, 1449000, 4, 2, 7, 1, 2, 1, 0, '10-Feb-2025'), 
 'detached', 2010, '1 york lane', 'M1K2G3', 40, 104));
 
+-- insertion format that worked for me (mj):
+insert into property values(property_t('d02', (SELECT REF(r) FROM region r WHERE r.rid = 'r001'), listing_t('l012', DATE '2024-10-10', 2010, 2695000.00, 4, 2, 7, 1, 2, 1, 0, DATE '2025-10-02'), 'detached', 2010, '1 york lane', 'M1K2G3', 40.2, 104.1));
+
 insert into property values(property_t('s02', 'r001', 'semidetached', '2005', '94 cook rd', 'M2K3G4', 24, 107));
 insert into property values(property_t('c03', 'r001','condo', '2021', '47 university rd', 'M3K4G5', 27, 28));
 insert into property values(property_t('s04', 'r004', 'semidetached', '1950', '57 alberta ave', 'M4K5G6', 17, 133));
