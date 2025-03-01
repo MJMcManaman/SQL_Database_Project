189422455c6455bf3be110ddc23fbfddbadb79f2
JinxiaoZhou <jinxiao.zhou@juphoon.com>
2025-02-28 19:46:00 -0500

    fix region out of bondery

--- Mls2_Insertion.sql
+++ Mls2_Insertion.sql
@@ -2,6 +2,7 @@ insert into region values(region_t('r001', 'Central', 'Ontario', 'Toronto'));
 insert into region values(region_t('r002', 'West', 'British Columbia', 'Vancouver'));
 insert into region values(region_t('r003', 'East', 'Quebec', 'Montreal'));
 insert into region values(region_t('r004', 'Prairie', 'Alberta', 'Calgary'));
+insert into region values(region_t('r005', 'Atlantic', 'Nova Scotia', 'Halifax'));
 
 --------------------------------------------------------------------------------------
 
@@ -12,7 +13,7 @@ listing_t('l001', DATE '2024-10-10', 2695000.00, 4, 2, 7, 1, 2, 1, 0, DATE '2025
 'detached', 2010, '1 york lane', 'M1K2G3', 40.2, 104.1));
 insert into property values(property_t('s02', (SELECT REF(r) FROM region r WHERE r.rid = 'r001'), listing_t('l002', DATE '2025-01-05', 1449000.00, 1, 1, 2, 1, 2, 2, 0, DATE '2025-02-01'), 'semidetached', 2005, '94 cook rd', 'M2K3G4', 24.5, 107.0));
 insert into property values(property_t('c03', (SELECT REF(r) FROM region r WHERE r.rid = 'r004'), listing_t('l003', DATE '2024-11-19', 1240000.00, 3, 1, 5, 3, 1, 4, 0, DATE '2025-02-21'), 'condo', 2021, '47 university rd', 'M3K4G5', 27.2, 28.5));
-insert into property values(property_t('s04', (SELECT REF(r) FROM region r WHERE r.rid = 'r005'), listing_t('l004', DATE '2024-10-01', 798000.00, 2, 2, 3, 0, 1, 2, 1, DATE '2025-03-02'), 'semidetached', 1960, '57 alberta ave', 'M4K5G6', 17.4, 133.6));
+insert into property values(property_t('s04', (SELECT REF(r) FROM region r WHERE r.rid = 'r003'), listing_t('l004', DATE '2024-10-01', 798000.00, 2, 2, 3, 0, 1, 2, 1, DATE '2025-03-02'), 'semidetached', 1960, '57 alberta ave', 'M4K5G6', 17.4, 133.6));
 insert into property values(property_t('s05', (SELECT REF(r) FROM region r WHERE r.rid = 'r005'), listing_t('l005', DATE '2024-10-05', 1425000.00, 4, 2, 6, 4, 2, 3, 0, DATE '2025-02-12'), 'detached', 1978, '25 ascot ave', 'M6K7G8', 30.0, 107.0));
 
 --------------------------------------------------------------------------------------
--- display.sql
+++ display.sql
@@ -26,3 +26,7 @@ select p.propertyDetail.openHouse from property p  where p.pid = 's02';
 
 -- selecting region id from property
 select p.roid.rid from property p where p.pid = 's02'
+
+-- shown region in property
+select p.roid.rid, p.roid.regionName, p.roid.province, p.roid.city, p.propertyDetail.lid, p.propertyDetail.listingStartDate, p.propertyDetail.listedPrice, p.propertyDetail.washroomNum, p.propertyDetail.livingroomNum, p.propertyDetail.bedroomNum, p.propertyDetail.balcony, p.propertyDetail.kitchenNum, p.propertyDetail.parkingSpace, p.propertyDetail.elevator, p.propertyDetail.openHouse, p.propertyType, p.builtYear, p.address, p.postalCode, p.propertyWidth, p.propertyLength
+from property p;
