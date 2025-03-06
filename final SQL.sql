644860bed3fae1d05e562c16c323a80877a6351b
JinxiaoZhou <johnnyzhoujinxiao@gmail.com>
2025-03-05 19:54:55 -0500

    works with no foregin key

--- final SQL.sql
+++ final SQL.sql
@@ -123,16 +123,16 @@ Create type agentContract_t as object(
 --Creat table 
 Create table region of region_t(rid primary key);
 Create table listing of listing_t (lid primary key);
-Create table property of property_t(pid primary key, foreign key (roid) references region);
+Create table property of property_t(pid primary key);
 Create table agent of agent_t(aid primary key);
 Create table customer of customer_t(cid primary key);
 Create table buyer of buyer_t(cid primary key);
 Create table seller of seller_t(cid primary key);
 Create table landlord of landlord_t(cid primary key);
 Create table tenant of tenant_t(cid primary key);
-Create table saleContract of saleContract_t(scid primary key, foreign key (buyerid) references buyer, foreign key (sellerid) references seller);
-Create table rentContract of rentContract_t(rcid primary key, foreign key (landlordid) references landlord, foreign key (tenantid) references tenant);
-Create table agentContract of agentContract_t(acid primary key, foreign key (aoid) references agent, foreign key (poid) references property, foreign key (coid) references customer, foreign key (scoid) references saleContract, foreign key (rcoid) references rentContract);
+Create table saleContract of saleContract_t(scid primary key);
+Create table rentContract of rentContract_t(rcid primary key);
+Create table agentContract of agentContract_t(acid primary key);
 
 
 
