35a033b7329e8cf22cb347f780c798a78f926d77
MJMcManaman <158525695+MJMcManaman@users.noreply.github.com>
2025-03-11 13:56:11 -0400

    Ml2_tableCreation.sql

---
+++ Ml2_tableCreation.sql
@@ -0,0 +1,12 @@
+Create table region of region_t(rid primary key);
+Create table listing of listing_t (lid primary key);
+Create table property of property_t(pid primary key);
+Create table agent of agent_t(aid primary key);
+Create table customer of customer_t(cid primary key);
+Create table buyer of buyer_t(cid primary key);
+Create table seller of seller_t(cid primary key);
+Create table landlord of landlord_t(cid primary key);
+Create table tenant of tenant_t(cid primary key);
+Create table saleContract of saleContract_t(scid primary key);
+Create table rentContract of rentContract_t(rcid primary key);
+Create table agentContract of agentContract_t(acid primary key);
