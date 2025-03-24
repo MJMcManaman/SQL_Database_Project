ddc6ad190acc38eddb19a14c02b44a47f80eba28
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-03-24 16:24:29 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -1,4 +1,6 @@
 1. List the number of detached, semi-detatched and detatched homes in each region.
 
-  SELECT XMLROOT(XMLELEMENT("Properties", XMLAGG(XMLELEMENT("Region", XMLATTRIBUTES(r.regionName AS "Region", XMLFOREST(p.propertyType = "Condo" AS "Number of Condos",
-  p.propertyType = "Semi
+  SELECT XMLROOT(XMLELEMENT("Properties", XMLAGG(XMLELEMENT("Region", XMLATTRIBUTES(r.regionName AS "Region", XMLFOREST(COUNT(p.propertyType = "condo" AS "Numberof Condos",
+  p.propertyType = "semidetached" AS "Number of semidetached", p.propertyType = "detached" AS "Number of detached houses"))))))), VERSION '1.0') 
+  AS document 
+  FROM property p, region r  
