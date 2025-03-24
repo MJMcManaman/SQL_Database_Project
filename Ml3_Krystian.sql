2242b6640da96d82e99c9d5a0434c51e8098f6cd
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-03-24 16:17:07 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -1,1 +1,4 @@
-1. List all properties in the Calgary region
+1. List the number of detached, semi-detatched and detatched homes in each region.
+
+  SELECT XMLROOT(XMLELEMENT("Properties", XMLAGG(XMLELEMENT("Region", XMLATTRIBUTES(r.regionName AS "Region", XMLFOREST(p.propertyType = "Condo" AS "Number of Condos",
+  p.propertyType = "Semi
