0345d1983dca75fff4ec5e22320420d5de8dd019
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-03-26 16:22:40 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -14,3 +14,20 @@ SELECT XMLROOT(
   WHERE p.roid.rid = r.rid
   GROUP BY r.regionName;
 
+
+2. List the price of each listing from greatest to least
+         
+SELECT XMLROOT(
+         XMLELEMENT("RegionPrice",
+            XMLAGG(
+                  XMLELEMENT("Region",
+                             XMLELEMENT("ID", l.lid),
+                             XMLFOREST(r.regionName AS "Region_type", l.listedPrice AS "ListedPrice")))
+         ), VERSION '1.0'
+         ) AS document
+FROM listing l
+WHERE l.roid.rid; 
+ORDER BY l.listedPrice DESC;
+
+
+                           
