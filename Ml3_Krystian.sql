6691959dc8b2d87b4738838e2abbc842edc39597
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-03-26 16:35:54 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -22,12 +22,12 @@ SELECT XMLROOT(
             XMLAGG(
                   XMLELEMENT("Region",
                              XMLELEMENT("ID", l.lid),
-                             XMLFOREST(r.regionName AS "Region_type", l.listedPrice AS "ListedPrice")))
-         ), VERSION '1.0'
+                             XMLFOREST(r.regionName AS "Region_type", l.listedPrice AS "ListedPrice")))),
+         VERSION '1.0'
          ) AS document
-FROM listing l
-WHERE l.roid.rid; 
-ORDER BY l.listedPrice DESC;
+FROM listing l, region r
+WHERE l.roid.rid 
+ORDER BY listedPrice DESC;
 
 
                            
