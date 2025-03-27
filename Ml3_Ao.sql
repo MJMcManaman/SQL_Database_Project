3d7a9a31dc4448a4d6631997d2c3d1fd01057f6f
windswift2 <124313370+windswift2@users.noreply.github.com>
2025-03-26 21:10:04 -0400

    Ml3_Ao.sql

--- Ml3_Ao.sql
+++ Ml3_Ao.sql
@@ -1,1 +1,15 @@
+1. list all angents who have more than 12 years of experience  and their contracts numbers
 
+2. list all buyers' information who buy house before year 2025
+
+3. list all properties that not handled by agents with their own details
+
+  
+1~ 
+
+  
+1` calculate the average price of the all rented properties
+
+2` no one interests in the seventh property because of expensive, so its price is updated 
+
+3`
