644b4d997f42b419353cbcbe2eeb3a19a3e7f014
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-03-28 15:11:05 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -1,30 +1,32 @@
 1. List the number of detached, semi-detatched and detatched homes in each region.
 
 SELECT XMLROOT(
-         XMLELEMENT("Properties",
+         XMLELEMENT("Properties_Sold_By_Types",
             XMLAGG(
-               XMLELEMENT("Region",
-                  XMLATTRIBUTES(r.regionName AS "Region"),
-                  XMLELEMENT("TotalProperties", COUNT(p.propertyType))
+               XMLELEMENT("Property_Type",
+                  XMLATTRIBUTES(ac.poid.propertyType AS "Type"),
+                  XMLFOREST(COUNT(ac.poid.propertyType) AS "Number_of_Properties")
                )
             )
-         ), VERSION '1.0'
-       ) AS document
-  FROM property p, region r
-  WHERE p.roid.rid = r.rid
-  GROUP BY r.regionName;
+         ),
+         VERSION '1.0'
+       ) AS doc
+FROM agentContract ac
+GROUP BY ac.poid.propertyType;
 
 
+2. List the number of detached, semi-detatched and detatched homes in each region.
 
+         
 SELECT XMLROOT(
          XMLELEMENT("Properties",
             XMLAGG(
                XMLELEMENT("Region",
                   XMLATTRIBUTES(r.regionName AS "Region"),
                   XMLFOREST(
-                     COUNT(CASE WHEN p.propertyType = 'detached' THEN 1 END) AS "DetachedHomes",
-                     COUNT(CASE WHEN p.propertyType = 'semi-detached' THEN 1 END) AS "SemiDetachedHomes",
-                     COUNT(CASE WHEN p.propertyType = 'detatched' THEN 1 END) AS "DetatchedHomes"
+                     COUNT(CASE WHEN p.propertyType = 'condo' THEN 1 END) AS "condo",
+                     COUNT(CASE WHEN p.propertyType = 'semidetached' THEN 1 END) AS "semidetached",
+                     COUNT(CASE WHEN p.propertyType = 'detatched' THEN 1 END) AS "detached"
                   )
                )
             )
@@ -34,21 +36,7 @@ SELECT XMLROOT(
   WHERE p.roid.rid = r.rid
   GROUP BY r.regionName;
 
-SELECT XMLROOT(
-         XMLELEMENT("Properties",
-            XMLAGG(
-               XMLELEMENT("Region",
-                  XMLATTRIBUTES(r.regionName AS "Region"),
-                  XMLELEMENT("DetachedHomes", COUNT(CASE WHEN p.propertyType = 'detached' THEN 1 END)),
-                  XMLELEMENT("SemiDetachedHomes", COUNT(CASE WHEN p.propertyType = 'semi-detached' THEN 1 END)),
-                  XMLELEMENT("DetatchedHomes", COUNT(CASE WHEN p.propertyType = 'detatched' THEN 1 END)) 
-               )
-            )
-         ), VERSION '1.0'
-       ) AS document
-  FROM property p, region r
-  WHERE p.roid.rid = r.rid
-  GROUP BY r.regionName;
+
 
 
 2. List the price of each listing from greatest to least
@@ -65,21 +53,9 @@ FROM listing l, region r
 WHERE l.roid.rid 
 ORDER BY listedPrice DESC;
 
+
+
 3. 
-
-SELECT XMLROOT(XMLELEMENT("Contracts", 
-         XMLELEMENT("Sale Contract", 
-         XMLAGG(XMLELEMENT("Contract Details",
-                  XMLATTRIBUTES(ac.scoid.scid AS "SCID"),
-         XMLFOREST(ac.scoid.buyerid.cname AS "Buyer Name", 
-         ac.scoid.sellerid.cname AS "Seller Name", 
-         ac.scoid.salePrice AS "Sale Price", 
-         ac.poid.address AS "Property Address",
-         ac.scoid.signedTime AS "Signed Date"))))), version '1.0')
-         AS document
-         FROM agentContract ac 
-
-4.
 SELECT XMLROOT(
          XMLELEMENT("BuyerWithProperty", 
            XMLAGG(
@@ -94,3 +70,32 @@ SELECT XMLROOT(
        ) AS document
 FROM buyer b
 WHERE b.cid IS NOT NULL;
+
+
+
+
+4. 
+SELECT XMLELEMENT(
+    "Property_Sold_Price_in_Region",
+    XMLATTRIBUTES(ac.poid.pid AS "Property_id"),
+    XMLELEMENT("Price", ac.scoid.salePrice),
+    XMLELEMENT("Age", ac.poid.age()),
+    XMLELEMENT("Size", ac.poid.propertySize())
+) AS document
+FROM agentContract ac
+WHERE ac.poid.roid.city = 'Halifax';
+
+
+3. 
+
+SELECT XMLROOT(XMLELEMENT("Contracts", 
+         XMLELEMENT("Sale Contract", 
+         XMLAGG(XMLELEMENT("Contract Details",
+                  XMLATTRIBUTES(ac.scoid.scid AS "SCID"),
+         XMLFOREST(ac.scoid.buyerid.cname AS "Buyer Name", 
+         ac.scoid.sellerid.cname AS "Seller Name", 
+         ac.scoid.salePrice AS "Sale Price", 
+         ac.poid.address AS "Property Address",
+         ac.scoid.signedTime AS "Signed Date"))))), version '1.0')
+         AS document
+         FROM agentContract ac 
