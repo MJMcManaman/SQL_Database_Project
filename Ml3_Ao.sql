1f4651e74c660df11d4d07b2245e89984dd50ecf
MJMcManaman <158525695+MJMcManaman@users.noreply.github.com>
2025-03-29 18:14:21 -0400

    Update Ml3_Ao.sql

--- Ml3_Ao.sql
+++ Ml3_Ao.sql
@@ -88,7 +88,12 @@ OracleXML getXML -user "grp2/here4grp2" -conn "jdbc:oracle:thin:@sit.itec.yorku.
     s.cname as sellerName, s.phoneNum as sellerPhone, s.emailAddress as sellerEmail, s.timeOwned as owningDate
     sc.sellerid as sellerID, sc.scid as saleContractID, sc.salePrice as propertyPrice
 FROM saleContract_t sc, customer_t s WHERE sc.sellerid = s.cid AND value(s) is of (only seller_t) AND s.timeOwned < DATE '2018-01-01'"
---values return objects' row, is of only condition to subtype
+-- correct version:
+OracleXML getXML -user "mj315915/wangyikai2001" -conn 
+"jdbc:oracle:thin:@sit.itec.yorku.ca:1521/studb10g" "SELECT DISTINCT sc.sellerid.cname as sellerName, 
+sc.sellerid.phoneNum as sellerPhone, sc.sellerid.emailAddress as sellerEmail, sc.sellerid.dateOwned as owningDate, 
+sc.sellerid.cid as sellerID, sc.scid as saleContractID, sc.salePrice as propertyPrice
+FROM saleContract sc WHERE sc.sellerid.dateOwned < DATE '2018-01-01'"
 
   
 -- 1` calculate the average price of the all rented properties
