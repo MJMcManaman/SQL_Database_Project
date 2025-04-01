35ed4a0777fbc815e779b98c777f9347c261c442
krys8987 <79266034+krys8987@users.noreply.github.com>
2025-04-01 10:01:23 -0400

    Update Ml3_Krystian.sql

--- Ml3_Krystian.sql
+++ Ml3_Krystian.sql
@@ -1,21 +1,8 @@
--- 1. List the number of detached, semi-detatched and detatched homes in each region.
--- this is Johnny's query I believe?????
-SELECT XMLROOT(
-         XMLELEMENT("Properties_Sold_By_Types",
-            XMLAGG(
-               XMLELEMENT("Property_Type",
-                  XMLATTRIBUTES(ac.poid.propertyType AS "Type"),
-                  XMLFOREST(COUNT(ac.poid.propertyType) AS "Number_of_Properties")
-               )
-            )
-         ),
-         VERSION '1.0'
-       ) AS doc
-FROM agentContract ac
-GROUP BY ac.poid.propertyType;
 
 -- modify so we don't get marks deducted:
 -- List all property's details, mostly concerned about the number of rooms and parking space.
+1. 
+
 SELECT XMLROOT(XMLELEMENT("Properties_Sold",
  XMLAGG(XMLELEMENT("Properties",
    XMLATTRIBUTES(ac.poid.pid AS "PID"),
@@ -28,27 +15,12 @@ VERSION '1.0') AS doc
 FROM agentContract ac WHERE ac.scoid IS NOT NULL;
 
 
--- 2. List the number of detached, semi-detatched and detatched homes in each region.      
-SELECT XMLROOT(
-         XMLELEMENT("Properties",
-            XMLAGG(
-               XMLELEMENT("Region",
-                  XMLATTRIBUTES(r.regionName AS "Region"),
-                  XMLFOREST(
-                     COUNT(CASE WHEN p.propertyType = 'condo' THEN 1 END) AS "condo",
-                     COUNT(CASE WHEN p.propertyType = 'semidetached' THEN 1 END) AS "semidetached",
-                     COUNT(CASE WHEN p.propertyType = 'detached' THEN 1 END) AS "detached"
-                  )
-               )
-            )
-         ), VERSION '1.0'
-       ) AS document
-FROM property p, region r
-WHERE p.roid.rid = r.rid
-GROUP BY r.regionName;
+
 
 -- fixed:
 -- problem: references was not introduced correctly.
+
+-- 2. List the number of detached, semi-detached and detached homes in each region.
 SELECT XMLROOT(XMLELEMENT("Properties",
  XMLAGG(XMLELEMENT("Region",
    XMLATTRIBUTES(p.roid.regionName AS "Region"),
@@ -61,19 +33,9 @@ FROM property p GROUP BY p.roid.regionName;
 
 
 
--- 3. List the price of each listing from greatest to least     
-SELECT XMLROOT(
-         XMLELEMENT("RegionPrice",
-            XMLAGG(
-                  XMLELEMENT("Region",
-                             XMLELEMENT("ID", l.lid),
-                             XMLFOREST(r.regionName AS "Region_type", l.listedPrice AS "ListedPrice")))),
-         VERSION '1.0'
-         ) AS document
-FROM listing l, region r
-WHERE l.roid.rid 
-ORDER BY listedPrice DESC;
 
+
+-- 3. List the price of each listing from greatest to least
 -- fixed:
 -- problem: ORDER BY should be GROUP BY? I'm not sure about why though; DESC isn't allowed in XML queries; 
 -- references was not introduced correctly, usage of XMLAGG was incorrectly.
@@ -87,21 +49,9 @@ SELECT XMLROOT(XMLELEMENT("Properties",
 FROM property p GROUP BY p.propertyDetail.listedPrice;
 
 
--- 4. List all buyers that has bought a property.
-SELECT XMLROOT(
-         XMLELEMENT("BuyerWithProperty", 
-           XMLAGG(
-             XMLELEMENT("Buyer", 
-               XMLFOREST(
-                 b.cname AS "Name",
-                 b.phoneNum AS "Phone")  
-             )
-           )
-         ), 
-         VERSION '1.0'
-       ) AS document
-FROM buyer b
-WHERE b.cid IS NOT NULL;
+
+
+--4. List all buyers that have bought a property
 -- fixed:
 -- small problem: because you are trying to list all the buyers that has a property,
 -- we should be referencing the buyer through the saleContract. Unless you wanted to list
@@ -124,27 +74,77 @@ WHERE sc.buyerid.cid IS NOT NULL;
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- (since we only need 3 xml queries for question 2, for the rest, I didn't double check, you can still use these query ideas
 -- to create XSU SQL query for question 3 and Xqueries for question 5).
+-----------------------------------
+--5. XSU
+$ OracleXML getXML -user "grp2/here4grp2" -conn "jdbc:oracle:thin:@sit.itec.yorku.ca:1521/studb10g" "select distinct ac.scoid.scid as SCID,
+         ac.scoid.buyerid.canme as Buyer_Name, ac.scoid.salePrice as Sale_Price, ac.poid.address as property_address, ac.scoid.signedTime
+         as Signed_Date from agentContract ac"
 
--- 5. 
-SELECT XMLELEMENT(
-    "Property_Sold_Price_in_Region",
-    XMLATTRIBUTES(ac.poid.pid AS "Property_id"),
-    XMLELEMENT("Price", ac.scoid.salePrice),
-    XMLELEMENT("Age", ac.poid.age()),
-    XMLELEMENT("Size", ac.poid.propertySize())
-) AS document
-FROM agentContract ac
-WHERE ac.poid.roid.city = 'Halifax';
 
--- 5.
- xquery
-  let $p := doc("/public/mj/property.xml")
-  for $property in $p/Properties/Property
-  where $property/address/text()
-  return $property/address/text()
-  /
+-- 6.
+xquery
+         let $p := doc("/public/mj/property.xml")
+         for $property in $p/Properties/Property
+         where $property/address/text()
+         return $property/address/text()
+         /
 
---6. 
+
+--7. 
+xquery
+         let $t := doc("/public/mj/tenant.xml")
+         let $rc := doc("/public/mj/rentContract.xml")
+         for $tenant in $t/Tenants/Tenant, 
+         $rentContract in $rc/RentContracts/RentContract
+         where $tenant/@tenantID = $rentContract/tenantID/text()
+         return 
+         <TenantRentInfo>
+         <tenantName>{$tenant/tenantName/text()}</tenantName>
+         <tenantID>{$rentContract/@rcid}</contractID>
+         </TenantRentInfo>
+         /
+
+--8.
+xquery
+         let $b := doc("/public/mj/buyer.xml")
+         for $c in $b/Buyers/Buyer
+         where $c/pricePreferred > "200000"
+         return $c
+         / 
+
+
+
+--- EXTRA -------------------------------------------------------------------------        
+3. 
+
+SELECT XMLROOT(XMLELEMENT("Contracts", 
+         XMLELEMENT("Sale Contract", 
+         XMLAGG(XMLELEMENT("Contract Details",
+                  XMLATTRIBUTES(ac.scoid.scid AS "SCID"),
+         XMLFOREST(ac.scoid.buyerid.cname AS "Buyer Name", 
+         ac.scoid.sellerid.cname AS "Seller Name", 
+         ac.scoid.salePrice AS "Sale Price", 
+         ac.poid.address AS "Property Address",
+         ac.scoid.signedTime AS "Signed Date"))))), version '1.0')
+         AS document
+         FROM agentContract ac 
+
+
+
+-- 6.
+xquery
+         let $t := doc("/public/mj/tenant.xml")
+         let $rc := doc("/public/mj/rentContract.xml")
+         for $tenant in $t/Tenants/Tenant, 
+         $rentContract in $rc/RentContracts/RentContract
+         where $tenant/@tenantID = $rentContract/tenantID/text()
+         return 
+         <TenantRentInfo>
+         <tenantName>{$tenant/tenantName/text()}</tenantName>
+         <tenantID>{$rentContract/@rcid}</contractID>
+         </Tenant
+
+--7. 
 xquery
           let $p := doc('/public/mj/property.xml')
           for $property in $p/Properties/Property
@@ -159,17 +159,13 @@ where xs:date($buyer/dateStarted) > xs:date("2018-01-01")
 return $buyer/@buyerID
 /
 
-
-3. 
-
-SELECT XMLROOT(XMLELEMENT("Contracts", 
-         XMLELEMENT("Sale Contract", 
-         XMLAGG(XMLELEMENT("Contract Details",
-                  XMLATTRIBUTES(ac.scoid.scid AS "SCID"),
-         XMLFOREST(ac.scoid.buyerid.cname AS "Buyer Name", 
-         ac.scoid.sellerid.cname AS "Seller Name", 
-         ac.scoid.salePrice AS "Sale Price", 
-         ac.poid.address AS "Property Address",
-         ac.scoid.signedTime AS "Signed Date"))))), version '1.0')
-         AS document
-         FROM agentContract ac 
+-- 5. 
+--SELECT XMLELEMENT(
+  --  "Property_Sold_Price_in_Region",
+   -- XMLATTRIBUTES(ac.poid.pid AS "Property_id"),
+   -- XMLELEMENT("Price", ac.scoid.salePrice),
+   -- XMLELEMENT("Age", ac.poid.age()),
+   -- XMLELEMENT("Size", ac.poid.propertySize())
+--) AS document
+--FROM agentContract ac
+--WHERE ac.poid.roid.city = 'Halifax';
